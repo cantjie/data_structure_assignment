7fbfd703a533b8632a7ec3f1e551c565d625d1dc
Cantjie <cantjie@163.com>
2017-10-29 09:09:01 +0800

    错误的操作了git‘

---
+++ week2/week2/week2.cpp
@@ -0,0 +1,24 @@
+// week2.cpp : 定义控制台应用程序的入口点。
+//
+
+//#include "stdafx.h"
+//
+//template <class Elem> class List {
+//public:
+//	virtual void clear() = 0;
+//	virtual bool IsEmpty(const Elem&) = 0;
+//	virtual bool IsFull(const Elem&) = 0;
+//	virtual bool insert(const Elem&) = 0;
+//	virtual bool remove(const Elem&) = 0;
+//	//virtual traverse() = 0;
+//	//virtual merge(Elem )
+//	
+//};
+//int main()
+//{
+//	//printf("hello world;\n");
+//    return 0;
+//}
+
+#include "stdafx.h"
+
